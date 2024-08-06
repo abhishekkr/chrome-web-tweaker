e6c4589175088c2cbb40711a1bc185ee3558b89b
AbhishekKr <abhikumar163@gmail.com>
2024-08-06 23:14:34 +0530

    fix (for bg-ssp & h12) whatever js was there; more css selectors
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- content.js
+++ content.js
@@ -1,10 +1,28 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-  let nodes = document.getElementsByTagName("div");
-  for (n in nodes) {
-    if (n.hasAttribute("")) {
-      n.innerHTML = "data-google-query-id";
+function remove_ads(){
+  let div_nodes = document.getElementsByTagName("div");
+  if (div_nodes.length > 0) {
+    for (n in div_nodes) {
+      if (n.className != undefined && n.className.includes("bg-ssp-")) {
+        console.log("****************************", n.className);
+        n.remove();
+      }
     }
+    console.log("hiding ads in div[bg-ssp-*] missed by css");
   }
-  console.log("hiding ads in div[data-google-query-id] missed by css");
+
+  let h12_nodes = document.getElementsByTagName("h12");
+  if (h12_nodes.length > 0) {
+    for (n in h12_nodes) {
+      if (n.hasAttribute("data-adunit")) {
+        n.remove();
+      }
+    }
+    console.log("hiding ads in h12[data-adunit] missed by css");
+  }
+  console.log("web-tweaker::remove_ads");
+}
+
+// remove_ads();
+window.addEventListener('load', (event) => {
+  remove_ads();
 });
