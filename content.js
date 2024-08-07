c62742800a5ea50ed20a985110bba11c2ef8df00
AbhishekKr <abhikumar163@gmail.com>
2024-08-07 10:41:36 +0530

    js for malicious domain urls; css for custom ad injection
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-function check_and_remove_from_selector(selector, check){
+function check_and_remove_from_tagname(selector, check){
   let tag_nodes = document.getElementsByTagName(selector);
   for (let i = 0; i < tag_nodes.length; ++i) {
     let n = tag_nodes[i];
@@ -11,21 +11,46 @@ function check_and_remove_from_selector(selector, check){
       console.log("****[ERROR] WEB-TWEAKER::", selector, " remove for", n);
     }
   }
+}
 
+function check_and_remove_from_selector(selector, check){
+  let tag_nodes = document.querySelectorAll(selector);
+  for (let i = 0; i < tag_nodes.length; ++i) {
+    let n = tag_nodes[i];
+    try {
+      if (check(n) === true) {
+        n.remove();
+        console.log("****[INFO] WEB-TWEAKER::", selector, " removed for", n);
+      }
+    } catch(err) {
+      console.log("****[ERROR] WEB-TWEAKER::", selector, " remove for", n);
+    }
+  }
 }
 
 
 function remove_ads(timer=0){
-  check_and_remove_from_selector("iframe", function(n){
+  check_and_remove_from_tagname("iframe", function(n){
       return n.style != undefined && n.style.display == "none";
     });
 
-  check_and_remove_from_selector("div", function(n){
+  check_and_remove_from_tagname("div", function(n){
       return n.className != undefined && n.className.includes("bg-ssp-");
     });
 
-  check_and_remove_from_selector("h12", function(n){
+  check_and_remove_from_tagname("h12", function(n){
       return n.hasAttribute("data-adunit");
+    });
+
+  check_and_remove_from_selector("[src],[href]", function(n){
+      let link = "";
+      if (n.hasAttribute("src")) {
+        link = n.getAttribute("src");
+      } else if (n.hasAttribute("href")) {
+        link = n.getAttribute("href");
+      }
+      return link.includes("shameful-leader.com") ||
+        link.includes("//equipmentapes.com");
     });
 
   console.log("web-tweaker::remove_ads");
